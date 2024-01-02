84481d62193a8528f20d22171cdf598e530123c7
Leon Sulfierry <leon.bio0@hotmail.com>
2024-01-02 16:34:45 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,13 @@
 #SBATCH -J molecular_dynamics       # Nome job
 #SBATCH --exclusive                 # Utilização exclusiva dos nós durante a execução do job
 
+# squeue -a -u $USER   - Ver jobs do usuario
+# squeue -a -p sequana_dockvs  - Ver jobs da fila
+# scancel jobid
+# salloc -p sequana_dockvs -J job_name --exclusive
+# ssh node_returned
+# sbatch run.sh
+
 # exibe os nos alocados para o Job
 echo $SLURM_JOB_NODELIST
 nodeset -e $SLURM_JOB_NODELIST
