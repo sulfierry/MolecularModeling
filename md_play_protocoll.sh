3db099855f13ea191ad05ccea585bb5347ec2329
Leon Sulfierry <leon.bio0@hotmail.com>
2024-01-02 16:21:50 -0300

    Update md_play_protocoll.sh

--- md_play_protocoll.sh
+++ md_play_protocoll.sh
@@ -13,3 +13,23 @@ rst_in=("5cc8.rst7" "minimization.rst" "relax-part1.rst" "relax-part2.rst" "rela
 rst_out=("minimization.rst" "relax-part1.rst" "relax-part2.rst" "relax-part3.rst" "relax-part4.rst" "pre-prod.rst" "production.rst")
 trajectories=("minimization.crd" "relax-part1.crd" "relax-part2.crd" "relax-part3.crd" "relax-part4.crd" "pre-prod.crd" "production.crd")
 outputs=("minimization.out" "relax-part1.out" "relax-part2.out" "relax-part3.out" "relax-part4.out" "pre-prod.out" "production.out")
+
+
+abort()
+{
+    echo >&2 '
+
+************************
+*** ABORTAR EXECUÇÃO ***
+************************
+'
+    echo "Erro inesperado..." >&2
+    exit 1
+}
+
+trap 'abort' 0
+
+set -e
+
+echo "Iniciando protocolo..."
+
