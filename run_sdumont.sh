d87c1b782dc9083a9346bdd95085a4a9187a1fb8
Leon Sulfierry <leon.bio0@hotmail.com>
2024-01-04 12:35:57 -0300

    Update run_sdumont.sh

--- run_sdumont.sh
+++ run_sdumont.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --nodes=1                   # Numero de nós 21 fila nvidia)
+#SBATCH --nodes=1                   # Numero de nós
 #SBATCH -p sequana_dockvs           # Fila (partition) a ser utilizada
 #SBATCH -J molecular_dynamics       # Nome job
 #SBATCH --exclusive                 # Utilização exclusiva dos nós durante a execução do job
