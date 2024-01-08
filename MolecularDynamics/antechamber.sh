bc7f5ac8f09a1c3d047ddccc9883611cac50c3ce
Leon Sulfierry <leon.bio0@hotmail.com>
2024-01-08 17:19:16 -0300

    Update antechamber.sh

--- MolecularDynamics/antechamber.sh
+++ MolecularDynamics/antechamber.sh
@@ -1,5 +1,5 @@
 # Executar a conversão
-# obabel -i pdb 6kpf_top1.pdb -o mol2 -O 6kpf_top1.mol2
+obabel -i pdb 6kpf_top1.pdb -o mol2 -O 6kpf_top1.mol2
 
 # Atribuir Tipos de Átomos:
 antechamber -i 6kpf_top1.mol2 -fi mol2 -o output.mol2 -fo mol2 -c bcc -s 2
