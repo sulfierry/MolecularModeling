b2b86961f4307991a2c548dc4c4a46aa39ac60bc
Leon Sulfierry <leon.bio0@hotmail.com>
2024-01-10 11:20:44 -0300

    Update antechamber.sh

--- MolecularDynamics/antechamber.sh
+++ MolecularDynamics/antechamber.sh
@@ -7,7 +7,7 @@ antechamber -i 6kpf_top1.mol2 -fi mol2 -o output.mol2 -fo mol2 -c bcc -s 2
 # Gerar Arquivo de Preparação (.frcmod - contém parâmetros adicionais necessários que podem não estar presentes no campo de força GAFF padrão)
 parmchk2 -i output.mol2 -f mol2 -o ligand.frcmod
 
-# tleap
+# tleap -f tleap.in
 # source leaprc.gaff
 # loadamberparams ligand.frcmod
 # lig = loadmol2 output.mol2
