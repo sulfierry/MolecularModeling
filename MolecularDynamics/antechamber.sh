1c7416ab3bca9938dc8a37577987fbdb6b6ea63a
Leon Sulfierry <leon.bio0@hotmail.com>
2024-01-11 16:51:00 -0300

    Update antechamber.sh

--- MolecularDynamics/antechamber.sh
+++ MolecularDynamics/antechamber.sh
@@ -4,6 +4,8 @@ obabel -i pdb 6kpf_top1.pdb -o mol2 -O 6kpf_top1.mol2
 # Atribuir Tipos de Átomos:
 antechamber -i 6kpf_top1.mol2 -fi mol2 -o output.mol2 -fo mol2 -c bcc -s 2
 
+# antechamber -i sustiva_new.pdb -fi pdb -o sustiva.mol2 -fo mol2 -c bcc -s 2
+
 # Gerar Arquivo de Preparação (.frcmod - contém parâmetros adicionais necessários que podem não estar presentes no campo de força GAFF padrão)
 parmchk2 -i output.mol2 -f mol2 -o ligand.frcmod
 
