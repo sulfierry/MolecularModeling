32d7cb178b88780f5c86c10382803c20e0ed419e
Leon Sulfierry <leon.bio0@hotmail.com>
2024-01-15 13:14:57 -0300

    Update verify_parcial_charge.py

--- CETENE/verify_parcial_charge.py
+++ CETENE/verify_parcial_charge.py
@@ -13,3 +13,13 @@ def soma_cargas_mol2(arquivo_mol2):
                 carga_total += carga
     
     return carga_total
+
+# Substitua 'molecula.mol2' pelo caminho do seu arquivo .mol2
+carga_total = soma_cargas_mol2('output_test.mol2')
+
+if carga_total > 0:
+    print(f"A carga total é positiva: {carga_total:.4f}")
+elif carga_total < 0:
+    print(f"A carga total é negativa: {carga_total:.4f}")
+else:
+    print("A carga total é neutra.")
