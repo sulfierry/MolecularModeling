282a9bd3142389539236448bfbff8915cbaa0f6b
Leon Sulfierry <leon.bio0@hotmail.com>
2024-01-23 11:04:41 -0300

    Update water_remove.sh

--- Molecular_dynamics/water_remove.sh
+++ Molecular_dynamics/water_remove.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
+# Verifica se o número correto de argumentos foi fornecido
+if [ "$#" -ne 3 ]; then
+    echo "Uso: $0 input.prmtop input.crd nome_output"
+    exit 1
+fi
+
+# Atribui os argumentos a variáveis
+input_prmtop=$1
+input_crd=$2
+output_base_name=$3
+
 total=7 # Número total de iterações
 echo "Iniciando o processo de remoção de água e íons..."
+
+# Criar a pasta "water_remov" se ela não existir
+mkdir -p water_remov
 
 for i in {4..10}
 do
     echo -n "[$i/$total] Processando pasta $i..."
 
-    # Definindo os parâmetros
-    parameters=$i/5cc8.prmtop
-    trajetoria=$i/production.crd
-    output=./water_remov/$i/5cc8_${i}_wr
+    # Define os caminhos de entrada e saída para cada iteração
+    parameters="./$i/$input_prmtop"
+    trajetoria="./$i/$input_crd"
+    output="./water_remov/$i/${output_base_name}_${i}"
+
+    # Criar subpasta dentro de "water_remov" se não existir
+    mkdir -p ./water_remov/$i
 
     # Criando e escrevendo no arquivo de configuração do cpptraj
     echo "parm $parameters" > cpptraj.in
