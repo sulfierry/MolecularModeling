df34cfc89b39276f809a92de6b10d20def8dbd09
Leon Sulfierry <leon.bio0@hotmail.com>
2024-01-23 11:01:04 -0300

    Update water_remove.sh

--- Molecular_dynamics/water_remove.sh
+++ Molecular_dynamics/water_remove.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
-# Uso: ./script.sh xxx.prmtop nome-da-trajetoria.mdcrd nome-para-os-outputs
+total=7 # Número total de iterações
+echo "Iniciando o processo de remoção de água e íons..."
 
-parameters=$1
-trajetoria=$2
-output=$3
+for i in {4..10}
+do
+    echo -n "[$i/$total] Processando pasta $i..."
 
-# Criando e escrevendo no arquivo de configuração do cpptraj
-echo "parm $parameters" > cpptraj.in
-echo "parm $parameters [top1]" >> cpptraj.in
-echo "trajin $trajetoria" >> cpptraj.in
-echo "autoimage" >> cpptraj.in
-echo "parmstrip :WAT,Cl-,Na+ parmindex 1" >> cpptraj.in
-echo "parmwrite out $output.prmtop parmindex 1" >> cpptraj.in
-echo "strip :WAT,Cl-,Na+" >> cpptraj.in
-echo "outtraj $output.pdb onlyframes 1" >> cpptraj.in
-echo "trajout $output.dcd" >> cpptraj.in
-echo "run" >> cpptraj.in
+    # Definindo os parâmetros
+    parameters=$i/5cc8.prmtop
+    trajetoria=$i/production.crd
+    output=./water_remov/$i/5cc8_${i}_wr
 
-# Executando cpptraj com o arquivo de configuração
-cpptraj < cpptraj.in
+    # Criando e escrevendo no arquivo de configuração do cpptraj
+    echo "parm $parameters" > cpptraj.in
+    echo "parm $parameters [top1]" >> cpptraj.in
+    echo "trajin $trajetoria" >> cpptraj.in
+    echo "autoimage" >> cpptraj.in
+    echo "parmstrip :WAT,Cl-,Na+ parmindex 1" >> cpptraj.in
+    echo "parmwrite out $output.prmtop parmindex 1" >> cpptraj.in
+    echo "strip :WAT,Cl-,Na+" >> cpptraj.in
+    echo "outtraj $output.pdb onlyframes 1" >> cpptraj.in
+    echo "trajout $output.dcd" >> cpptraj.in
+    echo "run" >> cpptraj.in
 
-# Removendo o arquivo de configuração
-rm cpptraj.in
+    # Executando cpptraj com o arquivo de configuração
+    cpptraj < cpptraj.in
+
+    # Removendo o arquivo de configuração
+    rm cpptraj.in
+
+    echo " Concluído."
+done
+
+echo "Processo concluído para todas as pastas."
