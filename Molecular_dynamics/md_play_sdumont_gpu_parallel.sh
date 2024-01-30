208ba9676dc15a6dd8b625a30487c92c7ec77d96
Leon Sulfierry <leon.bio0@hotmail.com>
2024-01-30 18:06:05 -0300

    Update md_play_sdumont_gpu_parallel.sh

--- Molecular_dynamics/md_play_sdumont_gpu_parallel.sh
+++ Molecular_dynamics/md_play_sdumont_gpu_parallel.sh
@@ -3,3 +3,13 @@
 #SBATCH -p sequana_dockvs          # Fila (partition) a ser utilizada
 #SBATCH -J molecular_dynamics      # Nome do job
 #SBATCH --exclusive                # Utilização exclusiva dos nós
+
+
+# Carregar o ambiente necessário
+source /scratch/dockvs/softwares/amber22/app/amber.sh
+
+# Configurações gerais
+export DO_CUDA="pmemd.cuda"
+export DO_PARALLEL="mpirun -np 48 sander.MPI"
+export PRMTOP="5cc8.prmtop"
+export RST7="5cc8.rst7"
