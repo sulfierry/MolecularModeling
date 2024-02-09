4c17a2b0fe2da8d0cedec20ee33e60536730ad5e
Leon Sulfierry <leon.bio0@hotmail.com>
2024-02-09 10:24:59 -0300

    Update calculate_rmsd.tcl

--- Molecular_dynamics/scripts/calculate_rmsd.tcl
+++ Molecular_dynamics/scripts/calculate_rmsd.tcl
@@ -6,35 +6,36 @@ set trajetoria "5cc8_wr_1.dcd"
 
 # Carregando a topologia e a trajetória
 mol new $topologia type parm7
-mol addfile $trajetoria type dcd first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
+mol addfile $trajetoria type dcd first 0 last -1 step 1 waitfor all
 
-# Selecionando os átomos do backbone e seus átomos pesados (C, CA, O, N)
-set selecao [atomselect top "backbone and (name C or name CA or name O or name N)"]
+# Selecionando os átomos do backbone e seus respectivos átomos pesados (C, CA, O, N)
+set selecao "backbone and (name C or name CA or name O or name N)"
 
 # Inicializa o arquivo de saída para os resultados do RMSD
 set arquivo_saida "rmsd_alinhado_resultados_corrigido.txt"
 set fileId [open $arquivo_saida "w"]
 
+# Prepara a estrutura de referência (primeiro frame)
+set ref [atomselect top "$selecao" frame 0]
+set refCoords [$ref get {x y z}]
+
 # Alinhando a trajetória e calculando o RMSD
 set totalFrames [molinfo top get numframes]
-set refCoords ""
 for {set frame 0} {$frame < $totalFrames} {incr frame} {
-    $selecao frame $frame
-    $selecao update
-    if {$frame == 0} {
-        # Guarda as coordenadas do primeiro frame para usar como referência
-        set refCoords [$selecao get {x y z}]
-    } else {
+    set sel [atomselect top "$selecao" frame $frame]
+    if {$frame > 0} {
         # Alinha cada frame com o primeiro frame
-        set moveMatrix [measure fit $selecao $refCoords]
-        $selecao move $moveMatrix
+        set moveMatrix [measure fit $sel $refCoords]
+        $sel move $moveMatrix
     }
     # Calcula o RMSD do frame alinhado em relação ao primeiro frame
-    set rmsd [measure rmsd $selecao $refCoords]
+    set rmsd [measure rmsd $sel $ref]
     puts $fileId "$frame $rmsd"
+    $sel delete
 }
 
-# Fechando o arquivo de saída
+# Fechamento da seleção de referência e do arquivo de saída
+$ref delete
 close $fileId
 
 # Finalizando o script
