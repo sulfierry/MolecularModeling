705bddc7fda4be4a6198348a4800e13cba724e24
Leon Sulfierry <leon.bio0@hotmail.com>
2024-02-09 16:56:58 -0300

    Update md_traj.py

--- Molecular_dynamics/scripts/md_traj.py
+++ Molecular_dynamics/scripts/md_traj.py
@@ -27,3 +27,35 @@ class MDTraj:
         print(f"RMSD data saved to {csv_path}")
         print("\n")
 
+    def analyze_rmsd_distribution(self):
+        from scipy.stats import shapiro
+        shapiro_test = shapiro(self.rmsd_results['RMSD'].values)
+        alpha = 0.05
+        if shapiro_test[1] < alpha:
+            print("Rejeitamos a hipótese nula: os dados não seguem uma distribuição normal.")
+        else:
+            print("Não rejeitamos a hipótese nula: os dados podem seguir uma distribuição normal.")
+        estatisticas_descritivas = self.rmsd_results['RMSD'].describe()
+        print(estatisticas_descritivas)
+        print("\n")
+        return estatisticas_descritivas
+
+    def select_representative_frames(self, estatisticas_descritivas):
+        valores_de_interesse = {
+            'min': estatisticas_descritivas['min'],
+            '25': estatisticas_descritivas['25%'],
+            '50': estatisticas_descritivas['50%'],
+            '75': estatisticas_descritivas['75%'],
+            'max': estatisticas_descritivas['max']
+        }
+        frames_selecionados = []
+        for descricao, valor in valores_de_interesse.items():
+            idx = (self.rmsd_results['RMSD'] - valor).abs().idxmin()
+            frame_selecionado = self.rmsd_results.iloc[idx]
+            frames_selecionados.append({
+                'frame': frame_selecionado['Frame'],
+                'rmsd': frame_selecionado['RMSD'],
+                'descricao': descricao
+            })
+        self.selected_frames = pd.DataFrame(frames_selecionados)
+
