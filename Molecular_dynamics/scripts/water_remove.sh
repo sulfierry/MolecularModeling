eb2ff574525450cdd1cf25e588ecbadb016ccda4
Leon Sulfierry <leon.bio0@hotmail.com>
2024-02-21 19:11:47 -0300

    Update water_remove.sh

--- Molecular_dynamics/scripts/water_remove.sh
+++ Molecular_dynamics/scripts/water_remove.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+
 # Verifica se o número correto de argumentos foi fornecido
 if [ "$#" -ne 3 ]; then
     echo "Uso: $0 input_name.prmtop input_name.crd output_name"
+    # ./water_remov.sh 5cc8.prmtop production.crd 5cc8_wr
+    # o srcipt automaticamente ira remover as aguas, criar novas pastas
+    # e salvar o outputs nas pastas, porem sem as aguas e ions!
     exit 1
 fi
 
@@ -11,13 +15,13 @@ input_prmtop=$1
 input_crd=$2
 output_base_name=$3
 
-total=10 # Número total de iterações
+total=7 # Número total de iterações
 echo "Iniciando o processo de remoção de água e íons..."
 
 # Criar a pasta "water_remov" se ela não existir
 mkdir -p water_remov
 
-for i in {1..10}
+for i in {4..10}
 do
     echo -n "[$i/$total] Processando pasta $i..."
 
