8d050eb26ca2e198e8eec8efbdbba32aacdd91bf
Leon Sulfierry <leon.bio0@hotmail.com>
2024-02-26 10:42:32 -0300

    Update vmd_h2o_cutoff.py

--- Molecular_dynamics/scripts/vmd_h2o_cutoff.py
+++ Molecular_dynamics/scripts/vmd_h2o_cutoff.py
@@ -1,3 +1,19 @@
 import os
 import commands
 import sys
+
+configurations = open(sys.argv[1], "r")
+configurations2 = configurations.readline()
+inputpdb = configurations.readline()
+inputpdb = inputpdb.replace("\n","")
+pdb = 1
+os.system("touch vmdscript.tcl")
+while pdb <= 1:
+        pdb = str(pdb)
+        #os.system("mv "+inputpdb+" x.pdb")
+        vmdscript = open("vmdscript.tcl","w")
+        vmdscript.write('mol new '+inputpdb+' \nset sel [atomselect top "protein or (water within 4 of protein)"]\n$sel writepdb 4_ang_'+inputpdb+'\nexit')
+        vmdscript.close()
+        os.system("vmd -dispdev none < vmdscript.tcl")
+        os.system("rm vmdscript.tcl")
+        print("All night long!")
