96b6c9757420611aa9cdaadb647dd5d8545aac5b
Leon Sulfierry <leon.bio0@hotmail.com>
2024-03-05 16:00:54 -0300

    Update near_residue_prevalence.py

--- Molecular_dynamics/scripts/near_residue_prevalence.py
+++ Molecular_dynamics/scripts/near_residue_prevalence.py
@@ -13,36 +13,6 @@ class ResiduesPrevalence:
         self.threshold = threshold
         self.color = color
 
-    def search_near_residue(self, topology, trajectory):
-        """Chama o VMD para gerar um arquivo com os resíduos próximos a um ligante."""
-        tcl_script_filename = 'search_near_residue.tcl'
-
-        tcl_script_content = f'''
-        mol load prmtop {topology}.prmtop dcd {trajectory}.dcd
-        set ligand_name "{self.ligand_name}"
-        set distance {self.distance}
-        set closer_residue [atomselect 0 "protein within $distance of resname $ligand_name"]
-        set outfile [open "residues_near_ligand.dat" w]
-        set num_frames [molinfo 0 get numframes]
-        for {{set frame_number 0}} {{$frame_number < $num_frames}} {{incr frame_number}} {{
-            $closer_residue frame $frame_number
-            $closer_residue update
-            set residues [$closer_residue get {{resname resid}}]
-            set unique_residues [lsort -unique $residues]
-            puts $outfile "Frame $frame_number: [llength $unique_residues] unique residues near $ligand_name"
-            foreach {{resname resid}} $unique_residues {{
-                puts $outfile "    Residue $resname $resid"
-            }}
-        }}
-        close $outfile
-        quit
-        '''
-
-        with open(tcl_script_filename, 'w') as file:
-            file.write(tcl_script_content)
-
-        subprocess.run(['vmd', '-dispdev', 'text', '-e', tcl_script_filename], check=True)
-        os.remove(tcl_script_filename)
 
     def process_data(self):
         """Processa o arquivo de dados para calcular a prevalência de cada resíduo."""
@@ -72,30 +42,10 @@ class ResiduesPrevalence:
         plt.ylabel('Prevalence (%)')
         plt.xticks(rotation=45, ha="right")
         plt.title('Amino Acid Prevalence Near Ligand')
+        plt.savefig('aac_prevalence.png')
         plt.tight_layout()
         plt.show()
 
-    @staticmethod
-    def save_frames_as_pdb(frames, topology, trajectory):
-        """Utiliza o VMD para selecionar e salvar uma lista de frames como arquivos PDB."""
-        tcl_script_content = f'''
-        mol load prmtop {topology}.prmtop dcd {trajectory}.dcd
-
-        foreach frame {{{' '.join(map(str, frames))}}} {{
-            animate goto $frame
-            set outfile [format "frame%06d.pdb" $frame]
-            animate write pdb $outfile
-        }}
-
-        quit
-        '''
-
-        tcl_script_filename = 'save_frames.tcl'
-        with open(tcl_script_filename, 'w') as file:
-            file.write(tcl_script_content)
-
-        subprocess.run(['vmd', '-dispdev', 'text', '-e', tcl_script_filename], check=True)
-        os.remove(tcl_script_filename)
 
 
     @classmethod
@@ -111,9 +61,6 @@ class ResiduesPrevalence:
         # Cria uma instância da classe com as configurações desejadas
         instance = cls(ligand_name, distance, threshold, color)
 
-        # Chama o VMD para gerar o arquivo com os resíduos próximos ao ligante
-        instance.search_near_residue(topology, trajectory)
-
         # Processa o arquivo de dados gerado
         instance.process_data()
 
@@ -122,10 +69,6 @@ class ResiduesPrevalence:
 
         # Plota os dados filtrados
         instance.plot_data()
-
-        # Exemplo de frames a serem salvos e chamada para save_frames_as_pdb
-        frames_to_save = [0, 10, 20]
-        cls.save_frames_as_pdb(frames_to_save, topology, trajectory)
 
 
 if __name__ == '__main__':
