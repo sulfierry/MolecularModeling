0353ec9199c73c054c2df1db3d5ff3d224ba2927
Leon Sulfierry <leon.bio0@hotmail.com>
2024-03-12 12:21:46 -0300

    Update search_near_residue.sh

--- Molecular_dynamics/scripts/search_near_residue.sh
+++ Molecular_dynamics/scripts/search_near_residue.sh
@@ -111,3 +111,111 @@ EOF
 }
 
 
+near_residue_prevalence() {
+    cat << EOF > near_residue_prevalence.py
+import os
+import re
+import subprocess
+import matplotlib.pyplot as plt
+from collections import defaultdict
+
+class ResiduesPrevalence:
+
+    def __init__(self, ligand_name, threshold=10, color='blue', root_dir='.'):
+        self.ligand_name = ligand_name
+        self.file_path = os.path.join(root_dir, 'residues_near_ligand_total.dat')
+        self.threshold = threshold
+        self.color = color
+        self.root_dir = root_dir
+
+    def merge_data_files(self):
+        """Realiza o merge dos arquivos residues_near_ligand.dat em todas as pastas."""
+        unique_residues_by_frame = defaultdict(set)
+
+        # Substitua [1, 11] pelo intervalo correto de pastas, se necessário
+        for i in range(${start_folder}, ${end_folder}+1):
+            folder_path = os.path.join(self.root_dir, str(i))
+            file_path = os.path.join(folder_path, "residues_near_ligand.dat")
+
+            if os.path.isfile(file_path):
+                with open(file_path, 'r') as file:
+                    current_frame = None
+                    for line in file:
+                        if line.startswith("Frame"):
+                            current_frame = line.split()[1].strip(':')
+                        else:
+                            residue_info = line.strip().split()[1:] # Remove 'Residue' e apenas extrai os resíduos
+                            for j in range(0, len(residue_info), 2):
+                                residue = f"{residue_info[j]} {residue_info[j+1]}"
+                                unique_residues_by_frame[current_frame].add(residue)
+
+        # Salvar os dados consolidados em um novo arquivo
+        with open(self.file_path, 'w') as output_file:
+            for frame, residues in sorted(unique_residues_by_frame.items(), key=lambda x: int(x[0])):
+                output_file.write(f"Frame {frame}: {len(residues)} unique residues near ligand\n")
+                for residue in sorted(residues):
+                    output_file.write(f"    Residue {residue}\n")
+
+
+    def process_data(self):
+        """Processa o arquivo de dados para calcular a prevalência de cada resíduo."""
+        residue_count = defaultdict(int)
+        with open(self.file_path, 'r') as file:
+            for line in file:
+                if line.startswith('    Residue '):
+                    residues = re.findall(r'([A-Z]+ \d+)', line)
+                    for residue in residues:
+                        residue_count[residue] += 1
+
+        total_frames = max(int(line.split()[1].rstrip(':')) for line in open(self.file_path) if line.startswith("Frame")) + 1
+        self.residue_prevalence = {residue: count / total_frames for residue, count in residue_count.items()}
+
+    def filter_by_threshold(self):
+        """Filtra os resíduos com prevalência acima do threshold especificado."""
+        self.filtered_data = {res: prevalence for res, prevalence in self.residue_prevalence.items() if prevalence * 100 >= self.threshold}
+
+    def plot_data(self):
+        """Gera o gráfico com os dados processados e filtrados, ordenados pelo número do resíduo."""
+        sorted_data = sorted(self.filtered_data.items(), key=lambda x: int(x[0].split()[1]))
+        labels = [f"{res[0].split()[0]} {res[0].split()[1]}" for res in sorted_data]
+        values = [prevalence * 100 for _, prevalence in sorted_data]
+        plt.figure(figsize=(10, 8))
+        plt.bar(labels, values, color=self.color)
+        plt.xlabel('Amino Acid and Number')
+        plt.ylabel('Prevalence (%)')
+        plt.xticks(rotation=45, ha="right")
+        plt.title('Amino Acid Prevalence Near Ligand')
+        plt.tight_layout()
+        plt.savefig('aac_prevalence.png')
+        plt.show()
+
+
+    @classmethod
+    def main(cls):
+        ligand_name = "${ligand_name}"
+        threshold = ${contact_threshold_percentage}
+        color = 'royalblue'
+        root_dir = '.'  # Caminho para o diretório raiz onde estão localizadas as pastas
+
+        # Cria uma instância da classe com as configurações desejadas
+        instance = cls(ligand_name, threshold, color, root_dir)
+
+        # Realiza o merge dos arquivos de dados
+        instance.merge_data_files()
+
+        # Processa o arquivo de dados gerado
+        instance.process_data()
+
+        # Filtra os dados com base no threshold especificado
+        instance.filter_by_threshold()
+
+        # Plota os dados filtrados
+        instance.plot_data()
+
+if __name__ == '__main__':
+    ResiduesPrevalence.main()
+
+EOF
+}
+
+main
