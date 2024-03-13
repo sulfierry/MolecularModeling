f54cac9a47651135d29fa47855892a1698c41ab2
Leon Sulfierry <leon.bio0@hotmail.com>
2024-03-13 12:28:38 -0300

    Update post_process_interactions.py

--- Molecular_dynamics/scripts/interactions/post_process_interactions.py
+++ Molecular_dynamics/scripts/interactions/post_process_interactions.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 class ProcessInteractions:
+
     def __init__(self, input_dir, output_file):
         self.input_dir = input_dir
         self.output_file = output_file
@@ -11,7 +12,8 @@ class ProcessInteractions:
     def reformat_data(self):
         dat_files = sorted(glob.glob(os.path.join(self.input_dir, 'detailed_interactions_frame_*.dat')))
         with open(self.output_file, 'w') as outfile:
-            outfile.write("frame\tligand_atom\tligand_atom_index\taminoacid\taminoacid_atom\taminoacid_number\n")
+            # Inclui o cabeçalho da coluna de distância
+            outfile.write("frame\tligand_atom\tligand_atom_index\taminoacid\taminoacid_atom\taminoacid_number\tdistance\n")
             for dat_file in dat_files:
                 with open(dat_file, 'r') as infile:
                     for line in infile:
@@ -22,9 +24,14 @@ class ProcessInteractions:
                         aminoacid = parts[9]
                         aminoacid_number = parts[10]
                         aminoacid_atom = parts[11]
-                        output_line = f"{frame}\t{ligand_atom}\t{ligand_atom_index}\t{aminoacid}\t{aminoacid_atom}\t{aminoacid_number}\n"
+                        # Extraí a distância do final da linha, assumindo que ela sempre aparece após "Distance:"
+                        distance = line.split('Distance: ')[1].strip()
+                        # Inclui a distância na linha de saída
+                        output_line = f"{frame}\t{ligand_atom}\t{ligand_atom_index}\t{aminoacid}\t{aminoacid_atom}\t{aminoacid_number}\t{distance}\n"
                         outfile.write(output_line)
         print(f"Processamento concluído. Todos os dados foram salvos em: {self.output_file}")
+
+
 
     def calculate_prevalence(self, prevalence_threshold):
         data = pd.read_csv(self.output_file, sep='\t')
