4d38e1376481ba4d18c94466a2dfa9217ecfa25b
Leon Sulfierry <leon.bio0@hotmail.com>
2024-03-14 18:43:47 -0300

    Update process_mol2.sh

--- PDB_manipulation/process_dockthor_output/process_mol2.sh
+++ PDB_manipulation/process_dockthor_output/process_mol2.sh
@@ -24,3 +24,37 @@ for mol2file in *.mol2; do
 done
 
 
+# Verifica se a pasta "pdb_complexed" existe; se não, cria-a
+mkdir -p pdb_complexed
+
+# Percorre todos os arquivos .pdb na pasta "mol2_to_pdb"
+for ligand_pdb in mol2_to_pdb/*.pdb; do
+  # Extrai o nome base do arquivo (sem a extensão .pdb)
+  base_name=$(basename "$ligand_pdb" .pdb)
+
+  # Cria um novo nome de arquivo para o complexo proteína-ligante, adicionando "_complexed" antes da extensão .pdb
+  complex_pdb_name="${base_name}_complexed.pdb"
+
+  # Caminho completo do novo arquivo complexo na pasta "pdb_complexed"
+  complex_pdb_path="pdb_complexed/$complex_pdb_name"
+
+  # Lê o conteúdo do arquivo PDB da proteína
+  protein_content=$(cat "$protein_pdb")
+
+  # Verifica se o conteúdo da proteína já termina com um registro TER; se não, adiciona
+  if ! echo "$protein_content" | grep -q "TER"; then
+    protein_content+="TER\n"
+  fi
+
+  # Lê o conteúdo do arquivo PDB do ligante
+  ligand_content=$(cat "$ligand_pdb")
+
+  # Combina os conteúdos, adicionando o ligante ao final da proteína
+  # e assegurando que o arquivo termina com "END"
+  echo -e "$protein_content\n$ligand_content\nEND" > "$complex_pdb_path"
+done
+
+
+# Verifica se a pasta "pdb_complexed_formated" existe; se não, cria-a
+mkdir -p pdb_complexed_formated
+
