857d05df971faec4b105e4268c271ef3059e997b
Leon Sulfierry <leon.bio0@hotmail.com>
2024-03-15 17:03:48 -0300

    Update vmd_distance_angle.tcl

--- PDB_manipulation/process_dockthor_output/vmd_distance_angle.tcl
+++ PDB_manipulation/process_dockthor_output/vmd_distance_angle.tcl
@@ -1,4 +1,23 @@
 vmd_angle_distance.sh#!/bin/bash
 
-# Diretório onde estão os arquivos .pdb
 
+PDB_DIR="pdb_frames"
+tcl_scipt="distance.tcl"
+input_file="distance.dat"
+output_file="distance_formatted.dat"
+
+# Lista os arquivos .pdb na ordem correta usando o comando ls com a opção -v
+for pdb_file in $(ls -v $PDB_DIR/*.pdb); do
+    # Extrai o nome base do arquivo sem a extensão .pdb
+    base_name=$(basename "$pdb_file" .pdb)
+    echo "Processando $base_name..."
+
+    # Executa o script VMD com o arquivo .pdb atual
+    vmd -dispdev text -e $tcl_scipt "$pdb_file"
+done
+
+echo "Todos os arquivos foram processados."
+
+# Usa awk para processar o arquivo e reescrever a primeira coluna com o número da linha
+awk '{print NR " " $0}' "$input_file" > "$output_file"
+echo "Arquivo formatado: $output_file"
