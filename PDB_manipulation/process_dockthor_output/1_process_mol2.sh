4171a96434d79c058f7087457534613313a21b56
Leon Sulfierry <leon.bio0@hotmail.com>
2024-03-18 10:41:59 -0300

    Update 1_process_mol2.sh

--- PDB_manipulation/process_dockthor_output/1_process_mol2.sh
+++ PDB_manipulation/process_dockthor_output/1_process_mol2.sh
@@ -1,80 +1,46 @@
 #!/bin/bash
 
-# executar dentro da pasta com os arquivos .mol2
-
 # Caminho para o arquivo PDB da proteína
 protein_pdb="../KP02043_5cc8_maestro_apo.pdb"
-
 
 global_index=1
 
 # Cria o diretório de saída se ele não existir
 mkdir -p mol2_to_pdb
 
-# Encontra todos os arquivos .mol2 na pasta atual e os processa um por um
-for mol2file in *.mol2; do
-  # Extrai o nome base do arquivo (sem a extensão)
-  base=$(basename "$mol2file" .mol2)
+# Ordenar e processar os arquivos .mol2 garantindo a sequência correta para cada run
+find . -maxdepth 1 -name '*.mol2' -print0 | sort -zV | while IFS= read -r -d '' mol2file; do
+    base=$(basename "$mol2file" .mol2)
 
-  # Usa o Open Babel para converter cada arquivo .mol2 para .pdb
-  obabel "$mol2file" -O "mol2_to_pdb/${base}_molecule_.pdb" -m
+    # Dividir o nome do arquivo para extrair as partes relevantes
+    IFS='_' read -ra ADDR <<< "$base"
+    docked_run="${ADDR[4]}"
+    molecule="${ADDR[6]}"
 
-  # Após a conversão, renomeia os arquivos gerados para incluir o índice global
-  for pdbfile in mol2_to_pdb/${base}_molecule_*.pdb; do
-    mv "$pdbfile" "mol2_to_pdb/${global_index}_${pdbfile#mol2_to_pdb/}"
-    let global_index++
-  done
+    # Gerar um nome base que reflete a ordem correta
+    new_base="${ADDR[0]}_${ADDR[1]}_${ADDR[2]}_${ADDR[3]}_${docked_run}_molecule_${molecule}"
+
+    # Usar o Open Babel para converter cada arquivo .mol2 para .pdb
+    obabel "$mol2file" -O "mol2_to_pdb/${global_index}_${new_base}.pdb" -m
+
+    ((global_index++))
 done
 
-
-# Verifica se a pasta "pdb_complexed" existe; se não, cria-a
+# A parte de criação do complexo proteína-ligante permanece a mesma
 mkdir -p pdb_complexed
 
-# Percorre todos os arquivos .pdb na pasta "mol2_to_pdb"
 for ligand_pdb in mol2_to_pdb/*.pdb; do
-  # Extrai o nome base do arquivo (sem a extensão .pdb)
-  base_name=$(basename "$ligand_pdb" .pdb)
+    base_name=$(basename "$ligand_pdb" .pdb)
 
-  # Cria um novo nome de arquivo para o complexo proteína-ligante, adicionando "_complexed" antes da extensão .pdb
-  complex_pdb_name="${base_name}_complexed.pdb"
+    complex_pdb_name="${base_name}_complexed.pdb"
+    complex_pdb_path="pdb_complexed/$complex_pdb_name"
 
-  # Caminho completo do novo arquivo complexo na pasta "pdb_complexed"
-  complex_pdb_path="pdb_complexed/$complex_pdb_name"
+    protein_content=$(cat "$protein_pdb")
+    ligand_content=$(cat "$ligand_pdb")
 
-  # Lê o conteúdo do arquivo PDB da proteína
-  protein_content=$(cat "$protein_pdb")
+    if ! echo "$protein_content" | grep -q "TER"; then
+        protein_content+="TER\n"
+    fi
 
-  # Verifica se o conteúdo da proteína já termina com um registro TER; se não, adiciona
-  if ! echo "$protein_content" | grep -q "TER"; then
-    protein_content+="TER\n"
-  fi
-
-  # Lê o conteúdo do arquivo PDB do ligante
-  ligand_content=$(cat "$ligand_pdb")
-
-  # Combina os conteúdos, adicionando o ligante ao final da proteína
-  # e assegurando que o arquivo termina com "END"
-  echo -e "$protein_content\n$ligand_content\nEND" > "$complex_pdb_path"
+    echo -e "$protein_content\n$ligand_content\nEND" > "$complex_pdb_path"
 done
-
-
-# Verifica se a pasta "pdb_complexed_formated" existe; se não, cria-a
-mkdir -p pdb_complexed_formated
-
-# Percorre todos os arquivos .pdb na pasta "pdb_complexed"
-for complexed_pdb in pdb_complexed/*.pdb; do
-    # Extrai o nome base do arquivo (sem a extensão .pdb)
-    base_name=$(basename "$complexed_pdb" .pdb)
-
-    # Define o caminho do arquivo de saída na pasta "pdb_complexed_formated"
-    formatted_pdb_path="pdb_complexed_formated/${base_name}_formated.pdb"
-
-    # Usa o pdb4amber para formatar o arquivo PDB e salva o resultado no arquivo de saída
-    pdb4amber -i "$complexed_pdb" -o "$formatted_pdb_path"
-done
-
-mkdir -p pdb4amber_infos
-*__nonprot.* pdb4amber_infos
-*_formated_renum.* pdb4amber_infos
-*formated_sslink* pdb4amber_infos
-mv -r pdb4amber_infos ../
