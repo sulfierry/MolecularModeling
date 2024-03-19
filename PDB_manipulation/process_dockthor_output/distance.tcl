ab912e9212568644b165447d77262a280a0ec425
Leon Sulfierry <leon.bio0@hotmail.com>
2024-03-19 16:27:05 -0300

    Update distance.tcl

--- PDB_manipulation/process_dockthor_output/distance.tcl
+++ PDB_manipulation/process_dockthor_output/distance.tcl
@@ -1,17 +1,14 @@
 set outfile [open "distance.dat" a]
-set n [molinfo top get numframes] 
+set n [molinfo top get numframes]
 set sum 0
 for {set i 0} {$i < $n} {incr i} {
-    set atom1 [atomselect top "resname LIG" frame $i] 
-    set atom2 [atomselect top "resid 378" frame $i] 
-    set com1 [measure center $atom1 weight mass]
-    set com2 [measure center $atom2 weight mass]
+    set atom1 [atomselect top "index 9736" frame $i]
+    set atom2 [atomselect top "resid 486" frame $i]
+    set com1 [measure center $atom1]
+    set com2 [measure center $atom2]
     set dis [veclength [vecsub $com1 $com2]]
     set sum [expr $sum + $dis]
     puts $outfile "$i  $dis"
 }
-set Avg_dis [expr $sum/$n]
-#puts $outfile "Avg Distance : $Avg_dis "
-#puts "Avg Distance : $Avg_dis "
 close $outfile
 quit
