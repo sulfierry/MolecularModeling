971bf800b379ee492d7e700353bf04d3afdf495c
Leon Sulfierry <leon.bio0@hotmail.com>
2024-03-20 10:38:54 -0300

    Update atomic_coordinate.tcl

--- PDB_manipulation/process_dockthor_output/atomic_coordinate.tcl
+++ PDB_manipulation/process_dockthor_output/atomic_coordinate.tcl
@@ -2,14 +2,16 @@ set outfile [open "coordinates.dat" a]
 set n [molinfo top get numframes]
 for {set i 0} {$i < $n} {incr i} {
     set atomSel [atomselect top "resname LIG" frame $i]
-    # Agora, também obtemos os nomes dos átomos
+    # Obtem as coordenadas, os nomes, os índices dos átomos e o índice do frame atual
     set coordsList [$atomSel get {x y z}]
     set atomNames [$atomSel get name]
-    foreach coords $coordsList name $atomNames {
+    set atomIndices [$atomSel get index]
+    foreach coords $coordsList name $atomNames index $atomIndices {
         # coords é uma lista de três elementos: x, y, z
-        # name é o nome do átomo
-        # Formata a saída como "x y z nome" e escreve no arquivo
-        puts $outfile "[join $coords " "] $name"
+        # name é o nome do átomo, e index é o índice do átomo
+        # i é o índice do frame atual
+        # Formata a saída como "frame x y z nome índice" e escreve no arquivo
+        puts $outfile "$i [join $coords " "] $name $index"
     }
 }
 close $outfile
