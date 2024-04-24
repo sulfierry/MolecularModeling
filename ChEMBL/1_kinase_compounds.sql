4f7c3ecf64b63f7e9a279d5403351ddee706799d
Leon Sulfierry <leon.bio0@hotmail.com>
2024-04-24 16:35:49 -0300

    Update 1_kinase_compounds.sql

--- ChEMBL/1_kinase_compounds.sql
+++ ChEMBL/1_kinase_compounds.sql
@@ -1,39 +1,11 @@
 -- psql -U leon -d chembl_33
-
-CREATE TABLE public.smile_kinase_manually_validated_kd_ki_ic50_10uM AS
-SELECT DISTINCT
-    d.chembl_id,
-    cs.molregno,
-    t.pref_name AS target_kinase,
-    cs.canonical_smiles,
-    act.standard_value,
-    act.standard_type,
-    act.pchembl_value,
-    d.pref_name AS compound_name,
-    o.organism AS organism
-FROM
-    compound_structures cs
-JOIN
-    activities act ON cs.molregno = act.molregno
-JOIN
-    assays a ON act.assay_id = a.assay_id
-JOIN
-    target_dictionary t ON a.tid = t.tid
-LEFT JOIN
-    molecule_dictionary d ON cs.molregno = d.molregno
-LEFT JOIN
-    component_sequences o ON t.component_id = o.component_id
-WHERE
-    t.pref_name LIKE '%kinase%' AND
-    cs.canonical_smiles IS NOT NULL AND
-    act.standard_type IN ('IC50', 'Ki', 'Kd') AND
-    act.standard_value IS NOT NULL AND
-    act.standard_units = 'nM' AND
-    act.standard_value < 10000 AND
-    act.standard_relation = '=' AND
-    (act.data_validity_comment IS NULL OR act.data_validity_comment = 'Manually validated');
-
-\COPY public.smile_kinase_manually_validated_kd_ki_ic50_10uM TO '/path/to/file/kinase_drug_info_all_manually_validated_IC50_Ki_kd_10uM.tsv' WITH (FORMAT csv, HEADER, DELIMITER E'\t');
+-- Criando a tabela kinase_group_activity_counts
+CREATE TABLE kinase_group_activity_counts AS
+SELECT kg.kinase_group, COUNT(*) AS count_kinase_group
+FROM kinase_groups kg
+JOIN assays a ON kg.tid = a.tid
+JOIN activities act ON a.assay_id = act.assay_id
+GROUP BY kg.kinase_group;
 
 
 CREATE TABLE public.smile_kinase_all_compounds AS
