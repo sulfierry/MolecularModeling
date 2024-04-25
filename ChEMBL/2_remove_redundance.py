c5d2d5eb8540dea63c2d13f5ba7c0b18459b122a
Leon Sulfierry <leon.bio0@hotmail.com>
2024-04-25 11:00:54 -0300

    Update 2_remove_redundance.py

--- ChEMBL/2_remove_redundance.py
+++ ChEMBL/2_remove_redundance.py
@@ -1,32 +1,33 @@
 import pandas as pd
 from rdkit import Chem
 from rdkit.Chem import SaltRemover
+from concurrent.futures import ThreadPoolExecutor
 from tqdm import tqdm
 
 class RemoveRedundance:
+
     def __init__(self, input_file_path, output_directory):
         self.input_file_path = input_file_path
         self.output_directory = output_directory
+        self.remover = SaltRemover.SaltRemover()
 
     def remove_salts_and_identify(self, smiles):
-        remover = SaltRemover.SaltRemover()
         mol = Chem.MolFromSmiles(smiles)
         if mol:
-            salt_free_mol = remover.StripMol(mol)
-            return Chem.MolToSmiles(salt_free_mol), salt_free_mol != mol
+            salt_free_mol = self.remover.StripMol(mol)
+            salt_free_smiles = Chem.MolToSmiles(salt_free_mol)
+            return salt_free_smiles, salt_free_smiles != smiles
         return smiles, False
+
+    def process_smiles(self, smiles_list):
+        with ThreadPoolExecutor(max_workers=4) as executor:
+            results = list(tqdm(executor.map(self.remove_salts_and_identify, smiles_list), total=len(smiles_list), desc="Removing salts"))
+        return results
 
     def process_data(self):
         data = pd.read_csv(self.input_file_path, sep='\t')
-        tqdm.pandas(desc="Removing salts")
-        results = data['canonical_smiles'].progress_apply(self.remove_salts_and_identify)
+        results = self.process_smiles(data['canonical_smiles'].tolist())
         data['canonical_smiles'], data['contains_salt'] = zip(*results)
-
-        return data
-
-    def create_salt_free_output(self, data):
-        tqdm.pandas(desc="Processing to salt-free SMILES")
-        data['canonical_smiles'] = data['canonical_smiles'].progress_apply(lambda x: self.remove_salts_and_identify(x)[0])
         return data
 
     def save_data(self, data, filename):
@@ -36,14 +37,11 @@ class RemoveRedundance:
         print("Processing data...")
         processed_data = self.process_data()
 
-        print("Creating salt-free output...")
-        salt_free_data = self.create_salt_free_output(processed_data)
-
         print("Saving all data files...")
         self.save_data(processed_data, 'nr_kinase_all_compounds_with_salt.tsv')
-        self.save_data(salt_free_data, 'nr_kinase_all_compounds_salt_free.tsv')
-        self.save_data(salt_free_data[salt_free_data['standard_value'] <= 10000], 'positive.tsv')
-        self.save_data(salt_free_data[salt_free_data['standard_value'] > 10000], 'negative.tsv')
+        self.save_data(processed_data, 'nr_kinase_all_compounds_salt_free.tsv')
+        self.save_data(processed_data[processed_data['standard_value'] <= 10000], 'positive.tsv')
+        self.save_data(processed_data[processed_data['standard_value'] > 10000], 'negative.tsv')
 
         print("Data processing and file generation completed.")
 
