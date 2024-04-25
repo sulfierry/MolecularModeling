6a169521fc6aa4b02c0d59efa5ce9f4deea9399b
Leon Sulfierry <leon.bio0@hotmail.com>
2024-04-25 16:19:11 -0300

    Update 2_remove_redundance.py

--- ChEMBL/2_remove_redundance.py
+++ ChEMBL/2_remove_redundance.py
@@ -49,6 +49,56 @@ class RemoveRedundance:
 
         print("Data processing and file generation completed.")
 
+
+class FormatChembl:
+    def __init__(self, input_file_path, pkidb_file, output_file_path):
+        self.input_file_path = input_file_path
+        self.pkidb_file = pkidb_file
+        self.output_file_path = output_file_path
+
+    @staticmethod
+    def process_kinase_group(group_name):
+        special_cases = {
+            "Enzyme": "Other", "Protein": "Other", "Other": "Other", "Unclassified": "Other",
+            "Atypical": "Other", "Kinase": "Other", "Nuclear e Other nuclear protein": "Nuclear protein",
+            "Membrane": "Membrane receptor", "Voltage-gated potassium channel": "Potassium channel",
+            "Tudor domain": "Tudor domain", "Bromodomain": "Bromodomain",
+            "Transcription factor": "Transcription factor", "Plant homeodomain": "Plant homeodomain",
+            "Protein Kinase": "Protein Kinase"
+        }
+        for key, value in special_cases.items():
+            if group_name.startswith(key):
+                return value
+        return group_name.split()[0] if group_name else group_name
+
+    def filter_pkidb_chembl_ids(self, input_data):
+        # Load PKIDB data
+        pkidb_data = pd.read_csv(self.pkidb_file, sep='\t')
+        # Extract CHEMBL_IDs from PKIDB
+        pkidb_ids = set(pkidb_data['CHEMBL_ID'])
+        # Filter out entries in input_data that are in PKIDB
+        return input_data[~input_data['chembl_id'].isin(pkidb_ids)]
+
+    def format_data(self):
+        # Load input data
+        data = pd.read_csv(self.input_file_path, sep='\t')
+        # Process kinase group names
+        if 'kinase_group' in data.columns:
+            data['kinase_group'] = data['kinase_group'].apply(self.process_kinase_group)
+        # Filter out CHEMBL_IDs from PKIDB
+        data = self.filter_pkidb_chembl_ids(data)
+        # Save formatted data
+        data.to_csv(self.output_file_path, sep='\t', index=False)
+        print("Processed data saved successfully.")
+
+def format_chembl_file():
+    file_path = '../0_database/kinase_all_compounds.tsv'
+    pkidb_file = '../0_database/pkidb/pkidb_2024-03-18.tsv'
+    output_file_path = '../0_database/kinase_all_compounds_formatted.tsv'
+    formatter = FormatChembl(file_path, pkidb_file, output_file_path)
+    formatter.format_data()
+
 if __name__ == "__main__":
-    remover = RemoveRedundance('../0_database/kinase_all_compounds.tsv', '.')
+    format_chembl_file()
+    remover = RemoveRedundance('../0_database/kinase_all_compounds_formatted.tsv', '.')
     remover.execute()
